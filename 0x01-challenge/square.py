e31e1cddbec83e8449df5293720aeb52eddec5d0
Dev-Kings <kingasiadavid41@gmail.com>
2024-07-23 19:10:34 +0300

    restarted

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,57 +1,33 @@
 #!/usr/bin/python3
-"""
-square module with functions to compute area and perimeter.
-"""
+""" Module for square class """
 
 
-class square:
-    """ Class square with area and perimeter calculation functions.
-    """
-    def __init__(self, **kwargs):
-        self.width = kwargs.get('width', 0)
-        self.height = self.width
+class square():
 
-    @property
-    def width(self):
-        """ Getting width"""
-        return (self._width)
+    width = 0
+    height = 0
 
-    @width.setter
-    def width(self, value):
-        """ Setting width"""
-        if value < 0:
-            raise ValueError("Width must be a non-negative value")
-        self._width = value
-        self._height = value
-
-    @property
-    def height(self):
-        """Getting value of height"""
-        return (self._height)
-
-    @height.setter
-    def height(self, value):
-        """ Setting height"""
-        if value < 0:
-            raise ValueError("Height must be a non-negative value")
-        self._width = value
-        self._height = value
+    def __init__(self, *args, **kwargs):
+        """ Class instantiation"""
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return (self.width * self.width)
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
-        """ Perimeter of the square """
-        return (self.width * 4)
+        """ Perimeter of square object """
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """String representation of class"""
-        return ("{}/{}".format(self.width, self.height))
+        """ String representation of square object """
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    s = square(width=12)
+    """ Creating a square instance """
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
